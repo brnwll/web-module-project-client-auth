3895469abd756682b278130a20f20b314fef1bfe
Brian Howell <brianhowell@pm.me>
2024-01-31 18:44:47 -0500

    Stub out header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Header from "./components/Header";
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function FriendList() {
+  return <h2>Friend List</h2>;
+}
+
+function AddFriend() {
+  return <h2>Add Friend</h2>;
+}
+
+function Logout() {
+  return <h2>Logout</h2>;
+}
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Header />
     </div>
   );
 }
