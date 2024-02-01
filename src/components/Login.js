71700a4867f65c2ec13df9d3a694c6aec072c6ff
Brian Howell <brianhowell@pm.me>
2024-01-31 19:11:20 -0500

    Add controlled inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value.toUpperCase(),
+    });
+  };
+
   return (
     <div className="container">
       <h2>Login</h2>
       <form>
         <label>Username</label>
-        <input type="text" />
+        <input type="text" name="username" onChange={handleChange} />
         <label>Password</label>
-        <input type="password" />
+        <input type="password" name="password" onChange={handleChange} />
         <button type="submit">Submit</button>
       </form>
     </div>
